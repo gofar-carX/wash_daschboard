36a8e88ae60372f6ed909fb46d150759d9bbff36
= <=>
2021-11-07 18:30:46 +0100

    pull

--- pages/api/Users/findOneUser.js
+++ pages/api/Users/findOneUser.js
@@ -5,8 +5,15 @@ const prisma = new PrismaClient({log: ["query"]})
 export default async function find(req,res){
   
    try {
-   
-     const User = await prisma.user_entity.findMany()
+     
+
+     const User = await prisma.user_entity.findUnique({
+         
+        where: {
+            id:3,
+          }
+        
+      })
     
     res.json(User)
      
