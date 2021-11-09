fc8550aaaa4015549adb119fb97d804a6df21a3d
= <=>
2021-11-09 09:17:28 +0100

    add worker, request

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -9,7 +9,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
@@ -18,7 +17,7 @@ import styles from "assets/jss/nextjs-material-dashboard/layouts/adminStyle.js";
 import bgImage from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
 
-let ps;
+let ps="AIzaSyDBj3vLiC4ndCkVof0Hzp8D-kZT8010lmI";
 
 export default function Admin({ children, ...rest }) {
   // used for checking current route
@@ -31,7 +30,6 @@ export default function Admin({ children, ...rest }) {
   // states and functions
   const [image, setImage] = React.useState(bgImage);
   const [color, setColor] = React.useState("white");
-  const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const handleImageClick = (image) => {
     setImage(image);
@@ -39,13 +37,7 @@ export default function Admin({ children, ...rest }) {
   const handleColorClick = (color) => {
     setColor(color);
   };
-  const handleFixedClick = () => {
-    if (fixedClasses === "dropdown") {
-      setFixedClasses("dropdown show");
-    } else {
-      setFixedClasses("dropdown");
-    }
-  };
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
@@ -101,14 +93,7 @@ export default function Admin({ children, ...rest }) {
           <div className={classes.map}>{children}</div>
         )}
         {getRoute() ? <Footer /> : null}
-        <FixedPlugin
-          handleImageClick={handleImageClick}
-          handleColorClick={handleColorClick}
-          bgColor={color}
-          bgImage={image}
-          handleFixedClick={handleFixedClick}
-          fixedClasses={fixedClasses}
-        />
+      
       </div>
     </div>
   );
